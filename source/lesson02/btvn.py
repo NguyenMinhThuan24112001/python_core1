c4944b0e86767e77024b35ec9f5d751c274034b7
NguyenMinhThuan24112001 <nguyenminhthuan24112001@gmail.com>
2021-11-13 17:50:30 +0700

    python

---
+++ source/lesson02/btvn.py
@@ -0,0 +1,17 @@
+""" BÀI TẬP VỀ NHÀ
+
+Bài 01. Trong lập trình python
+    - Đặt 5 tên biến đúng quy tắc
+    - Đặt 10 tên biến sai quy tắc
+
+Bài 02.
+    - Theo bài học thì có mấy kiểu dữ liệu?
+    - Ứng với mỗi kiểu đó, hãy khai báo 2 biến và dùng lệnh print để in ra màn hình
+
+Bài 03. Hãy lập trình đoạn chương trình Python giải quyết các yêu cầu sau
+    1. Yêu cầu người dùng nhập name của họ từ bàn phím.
+    2. In ra lời chào với họ đã đến chương trình
+    3. Yêu cầu họ nhập năm sinh
+    4. Tính toán và in ra số tuổi hiện tại của
+
+"""
